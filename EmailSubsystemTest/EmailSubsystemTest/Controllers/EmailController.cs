e8ee9712d363721217f8952aec991f64335c47d4
Unknown <arad18@student.monash.edu>
2017-07-29 16:25:19 +0200

    Creating email Subsystem

---
+++ EmailSubsystemTest/EmailSubsystemTest/Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmailSubsystemTest.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Form(string receiverEmail, string subject, string message)
+        {
+            try
+            {
+
+            }
+            catch(Exception)
+            {
+                ViewBag.Error = "There are some problems in sending email";
+            }
+
+            return View();
+        }
+    }
+}
